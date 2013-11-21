4a46828222d822b1005cd1ab712628a3d8d4b9bb
Anders Nygard <anygard@redbridge.se>
2013-11-21 15:53:50 +0100

    adding bash history settings

---
+++ dynamic.d/bash-history.sh
@@ -0,0 +1,21 @@
+# append instead of clobbering existing history
+shopt -s histappend
+
+# multi line commadn on one line
+shopt -s cmdhist
+
+# looooong history
+export HISTFILESIZE=1000000
+export HISTSIZE=1000000
+
+# ignore command starting with a ' ' and duplicate issuances of a command
+export HISTCONTROL=ignoreboth
+
+# ignore these commands
+#export HISTIGNORE='ls:bg:fg:history'
+
+# time stamps in .bash_history
+export HISTTIMEFORMAT='%F %T '
+
+#comitting history to disk on each prompt
+#export PROMPT_COMMAND='history -a'
