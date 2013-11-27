0a952ade6c6039a2d602f63d4ff42c99a622a792
Anders Nygard <anygard@redbridge.se>
2013-11-27 08:59:41 +0100

    starting point

--- dynamic.d/visual.d/lod256/prompt.sh
+++ dynamic.d/visual.d/lod256/prompt.sh
@@ -20,13 +20,37 @@
 source palette.sh
 
 # set a fancy prompt (non-color, unless we know we "want" color)
+
+# set xterm title?!
 function pcfunc {
     pwd=`echo ${PWD} | awk -v h="$HOME" '{gsub(h,"~") ; print}'`
     echo -ne "\033]0;${USER}@${HOSTNAME}: ${pwd}\007"
 }
 
+function auxps1_info {
+    AUXPS1_INFO=
+    DELIM=""
+    if [ ! -z $VIRTUAL_ENV ]; then
+	AUXPS1_INFO+="$(basename $VIRTUAL_ENV)"
+	DELIM=":"
+    fi
+
+    GITSTAT=$(git status --porcelain)
+    GITES=$?
+    if [ $GITES -eq 0 ]; then
+	BRANCH=$(git status | head -1 | cut -d' ' -f4)
+
+	if [ -z "$GITSTAT" ]; then
+	    COLOR=${GREEN}
+	else
+	    COLOR=${ORANGE}
+	fi
+	    AUXPS1_INFO+="${DELIM}\033[${COLOR}m${BRANCH}\033[00m"
+    fi
+}
+
 # appending array
-PROMPT_COMMAND_ARRAY+=('pcfunc')
+PROMPT_COMMAND_ARRAY+=('pcfunc' 'auxps1_info')
 
 
 case "$TERM" in
@@ -40,9 +64,8 @@ case "$TERM" in
 
 	EXITCODE=
 
-    #    export PS1='$(Q=$?; if [ "$Q" -eq "0" ]; then echo "\A";  else printf "\[\e[00;31m\]%05d\[\e[00m\]" $Q; fi) \j \[\033[00;32m\]\u@\h\[\033[00m\]:\[\033[00;94m\]\w\[\033[00m\]\$ '
-	#export PS1="\`Q=\$?; if [ \"\$Q\" -eq \"0\" ]; then echo \"\A\"; else printf \"\[\e[${CLERROR}m\]%05d\" $Q; fi\`\[\033[00m\] \j \[\033[${CLUSERHOST}m\]\u@\h\[\033[00m\]:\[\033[${CLPATH}m\]\w\[\033[00m\]\$\033]0;\u@\h:\w\007 "
-	export PS1="\`Q=\$?; if [ \"\$Q\" -eq \"0\" ]; then echo \"\A\"; else printf \"\[\e[${CLERROR}m\]%05d\" \"\$Q\"; fi\`\[\033[00m\] \j \[\033[${CLUSERHOST}m\]\u@\h\[\033[00m\]:\[\033[${CLPATH}m\]\w\[\033[00m\]\$ "
+	#export PS1="\`Q=\$?; if [ \"\$Q\" -eq \"0\" ]; then echo \"\A\"; else printf \"\[\e[${CLERROR}m\]%05d\" \"\$Q\"; fi\`\[\033[00m\] \j \[\033[${CLUSERHOST}m\]\u@\h\[\033[00m\]:\[\033[${CLPATH}m\]\w\[\033[00m\]\$ "
+	export PS1="\`Q=\$?; if [ \"\$Q\" -eq \"0\" ]; then echo \"\A\"; else printf \"\[\e[${CLERROR}m\]%05d\" \"\$Q\"; fi\`\[\033[00m\] \j \`if [ ! -z \"\$AUXPS1_INFO\" ]; then echo -e \"|\$AUXPS1_INFO|\"; fi\` \[\033[${CLUSERHOST}m\]\u@\h\[\033[00m\]:\[\033[${CLPATH}m\]\w\[\033[00m\]\$ "
 	;;
 
     *)
