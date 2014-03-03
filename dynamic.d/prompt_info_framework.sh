4916050fc1a2c62e98afab4a23bd3718869cbe65
Anders Nygård <anders.nygard@anygard.com>
2014-03-03 21:33:12 +0100

    Simplification, did away with the PROMPT_INFO_ARRAY

--- dynamic.d/prompt_info_framework.sh
+++ dynamic.d/prompt_info_framework.sh
@@ -6,24 +6,31 @@ export PROMPT_INFO_DATA_COL=115
 
 function prompt_info_framework {
     PROMPT_INFO_ARRAY=()
+    DELIM=""
     for f in ~/.CLIenv/dynamic.d/prompt_info_framework.d/*  ; do
         if [ -x $f ] ; then
-            source $f
+            BUFFER=$(source $f)
+            LABEL=$(echo $BUFFER | cut -d\\ -f1)
+            DATA=$(echo $BUFFER | cut -d\\ -f2)
+            echo -en "${DELIM}\033[38;5;${PROMPT_INFO_LABEL_COL}m${LABEL}\033[0m:\033[38;5;${PROMPT_INFO_DATA_COL}m${DATA}\033[0m"
+            DELIM=" "
         fi
-    done
-
-    #PROMPT_INFO_FRAMEWORK=""
-    DELIM=""
-    for ITEM in "${PROMPT_INFO_ARRAY[@]}"; do 
-        LABEL=$(echo $ITEM | cut -d\\ -f1)
-        DATA=$(echo $ITEM | cut -d\\ -f2)
-        #PROMPT_INFO_FRAMEWORK+="\e[${PROMPT_INFO_LABEL_COL}m]${LABEL}\e[00m]:\e[${PROMPT_INFO_DATA_COL}m]${DATA}\e[00m]${DELIM}"
-        echo -en "${DELIM}\033[38;5;${PROMPT_INFO_LABEL_COL}m${LABEL}\033[0m:\033[38;5;${PROMPT_INFO_DATA_COL}m${DATA}\033[0m"
-        DELIM=" "
     done
     if [[ $DELIM = " " ]]; then
         echo
     fi
+
+    #PROMPT_INFO_FRAMEWORK=""
+    #for ITEM in "${PROMPT_INFO_ARRAY[@]}"; do 
+    #    LABEL=$(echo $ITEM | cut -d\\ -f1)
+    #    DATA=$(echo $ITEM | cut -d\\ -f2)
+    #    #PROMPT_INFO_FRAMEWORK+="\e[${PROMPT_INFO_LABEL_COL}m]${LABEL}\e[00m]:\e[${PROMPT_INFO_DATA_COL}m]${DATA}\e[00m]${DELIM}"
+    #    echo -en "${DELIM}\033[38;5;${PROMPT_INFO_LABEL_COL}m${LABEL}\033[0m:\033[38;5;${PROMPT_INFO_DATA_COL}m${DATA}\033[0m"
+    #    DELIM=" "
+    #done
+    #if [[ $DELIM = " " ]]; then
+    #    echo
+    #fi
     #if [ "$PROMPT_INFO_FRAMEWORK" ]; then
     #    echo -e "$PROMPT_INFO_FRAMEWORK"
     #fi
